0c6354316a48ff92eedbe919a45650b091c4a4b3
C0C41N <0ct41n@gmail.com>
2020-09-29 22:42:07 +0500

    added close btn

--- src/app/add-clip/add-clip.module.ts
+++ src/app/add-clip/add-clip.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../shared/material.module';
 import { AddClipRoutingModule } from './add-clip-routing.module';
 import { AddClipComponent } from './add-clip.component';
 
 @NgModule({
 	declarations: [AddClipComponent],
-	imports: [CommonModule, AddClipRoutingModule],
+	imports: [CommonModule, AddClipRoutingModule, MaterialModule],
 })
 export class AddClipModule {}

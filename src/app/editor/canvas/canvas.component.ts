6dbaddce6a0b2ed08023ad7942a8477e717cb8a0
C0C41N <0ct41n@gmail.com>
2020-10-07 00:37:37 +0500

    uhhh

--- src/app/editor/canvas/canvas.component.ts
+++ src/app/editor/canvas/canvas.component.ts
@@ -1,3 +1,5 @@
+import { tap } from 'rxjs/operators';
+
 import { Component, OnInit } from '@angular/core';
 
 import { EditorService } from '../../core/editor.service';
@@ -10,9 +12,52 @@ import { EditorService } from '../../core/editor.service';
 export class CanvasComponent implements OnInit {
 	vids: string[];
 
+	video: HTMLVideoElement;
+	cTmp: HTMLCanvasElement;
+	ctxTmp: CanvasRenderingContext2D;
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D;
+
 	constructor(private service: EditorService) {}
 
 	ngOnInit(): void {
-		this.service.vidUrl.subscribe((e) => (this.vids = e));
+		this.service.vidUrl.pipe(tap((e) => (this.vids = e))).subscribe();
+	}
+
+	init(url: string): void {
+		console.log('init!');
+		console.log(this.vids);
+		this.canvas = document.querySelector('canvas');
+
+		this.ctx = this.canvas.getContext('2d');
+
+		this.cTmp = document.createElement('canvas');
+		this.cTmp.setAttribute('width', '200');
+		this.cTmp.setAttribute('height', '200');
+
+		this.ctxTmp = this.cTmp.getContext('2d');
+
+		this.video = document.querySelector('video');
+
+		this.computeFrame();
+	}
+
+	computeFrame(): void {
+		this.ctxTmp.drawImage(
+			this.video,
+			0,
+			0,
+			this.video.videoWidth,
+			this.video.videoHeight
+		);
+		console.log({ videoHeight: this.video.videoHeight });
+		const frame = this.ctxTmp.getImageData(
+			0,
+			0,
+			this.video.videoWidth,
+			this.video.videoHeight
+		);
+		this.ctx.putImageData(frame, 0, 0);
+		setTimeout(this.computeFrame, 250);
 	}
 }

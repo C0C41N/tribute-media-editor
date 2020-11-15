bef7938a169b0b13ac2bd66504a0d960a0bc5ce2
C0C41N <0ct41n@gmail.com>
2020-11-15 16:47:18 +0500

    - canvas, + img

--- src/app/editor/view/canvas/canvas.component.ts
+++ src/app/editor/view/canvas/canvas.component.ts
@@ -9,7 +9,7 @@ import { AfterViewInit, Component, OnInit, QueryList, ViewChildren } from '@angu
 	styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements OnInit, AfterViewInit {
-	vids: string[];
+	thumbnails: string[];
 
 	@ViewChildren('video')
 	video: QueryList<HTMLVideoElement>;
@@ -17,57 +17,9 @@ export class CanvasComponent implements OnInit, AfterViewInit {
 	@ViewChildren('canvas')
 	canvas: QueryList<HTMLCanvasElement>;
 
-	constructor(private service: EditorService) {}
+	constructor(private svc: EditorService) {}
 
-	ngOnInit(): void {
-		this.service.vidUrl.subscribe((e) => {
-			this.vids = e;
-			if (e[0]) {
-				console.log('beforeFetch');
-				fetch(e[0]).then((res) => {
-					console.log('fetch', e[0]);
-					res.arrayBuffer().then((b) => console.log('bin', b));
-				});
-			}
-		});
-	}
+	ngOnInit(): void {}
 
-	ngAfterViewInit(): void {
-		const vidAdded = combineLatest([this.video.changes, this.canvas.changes]);
-
-		vidAdded.subscribe((e) => {
-			const l = e.map((i: QueryList<any>) => i.toArray()[0].nativeElement);
-			const v: HTMLVideoElement = l[0];
-			const canvas: HTMLCanvasElement = l[1];
-
-			const context = canvas.getContext('2d');
-
-			v.addEventListener(
-				'play',
-				() => {
-					console.log('ok.');
-					const cw = v.videoWidth;
-					const ch = v.videoHeight;
-					canvas.width = cw / v.clientWidth;
-					canvas.height = ch / v.clientHeight;
-					const vRatio = (200 / v.videoHeight) * v.videoWidth;
-					const hRatio = (200 / v.videoWidth) * v.videoHeight;
-					setInterval(this.draw, 1000 / 30, v, context, vRatio, hRatio);
-				},
-				false
-			);
-		});
-	}
-
-	draw(
-		v: HTMLVideoElement,
-		context: CanvasRenderingContext2D,
-		cw: number,
-		ch: number
-	): void {
-		if (v.paused || v.ended) {
-			return;
-		}
-		context.drawImage(v, 0, 0, cw, ch);
-	}
+	ngAfterViewInit(): void {}
 }
